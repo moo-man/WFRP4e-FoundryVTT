d20996f4314be2d830e77abbc435a6c650fc84b3
Russell <russellthurman94@gmail.com>
2020-08-22 15:35:44 -0500

    Readded bug reporter

--- modules/apps/bug-report.js
+++ modules/apps/bug-report.js
@@ -3,11 +3,13 @@ export default class BugReportFormWfrp4e extends Application {
     constructor(app) {
         super(app)
 
-        this.endpoint = "http://localhost:3000/grievance"
+        this.endpoint = "https://imperial-post.herokuapp.com/grievance"
 
         this.domains = [
             "WFRP4e System",
-            "WFRP4e Content"
+            "WFRP4e Content",
+            "Starter Set",
+            "Rough Nights & Hard Days",
         ]
     }
 
@@ -20,7 +22,7 @@ export default class BugReportFormWfrp4e extends Application {
         options.height = 600;
         options.width = 600;
         options.minimizable = true;
-        options.title = "WFRP4e Bug Report"
+        options.title = "Post Your Grievance"
         return options;
     }
 
@@ -42,16 +44,30 @@ export default class BugReportFormWfrp4e extends Application {
                 body: data.description,
                 labels: ["bug"]
             })
-        }).then(res => console.log(res))
-            .catch(err => {
-                ui.notifications.error("Something went wrong.")
-                console.log(err)
-            })
+        })
+        .then(res => {
+            if (res.status == 201)
+            {
+                ui.notifications.notify("The Imperial Post Has Received Your Grievance! See the console for a link.")
+                res.json().then(json => {
+                    console.log("%c%s%c%s", 'color: gold', `IMPERIAL POST:`, 'color: unset', ` Thank you for your grievance submission. If you wish to monitor or follow up with additional details like screenshots, you can find your issue here: ${json.html_url}`)
+                })
+            }
+            else 
+            {
+               ui.notifications.error("The Imperial Post cannot receive your missive. Please see console for details.")
+               console.error(res)
+            }   
+
+        })
+        .catch(err => {
+            ui.notifications.error("Something went wrong.")
+            console.error(err)
+        })
     }
 
     activateListeners(html) {
         html.find(".bug-submit").click(ev => {
-            console.log(ev)
             let data = {};
             let form = $(ev.currentTarget).parents(".bug-report")[0];
             data.domain = $(form).find(".domain")[0].value
@@ -60,12 +76,11 @@ export default class BugReportFormWfrp4e extends Application {
             data.issuer = $(form).find(".issuer")[0].value
 
             data.title = `[${this.domains[Number(data.domain)]}] ${data.title}`
-            data.description = data.description + ` From: ${data.issuer}`
+            data.description = data.description + ` **From**: ${data.issuer}`
 
             if (!data.domain || !data.title || !data.description)
                 return ui.notifications.notify("Please fill out the form")
-            else
-                ui.notifications.notify("Thank you!")
+
             this.submit(data)
             this.close()
         })
--- modules/hooks/sidebar.js
+++ modules/hooks/sidebar.js
@@ -1,5 +1,7 @@
+import BugReportFormWfrp4e from "../apps/bug-report.js";
+
 export default function() {
-  /*Hooks.on("renderSidebarTab", async (app, html) => {
+  Hooks.on("renderSidebarTab", async (app, html) => {
     if (app.options.id == "settings")
     {
       let button = $("<button class='bug-report'>Report a Bug</button>")
@@ -11,5 +13,5 @@ export default function() {
       button.insertAfter(html.find(".game-system"))
       
     }
-  })*/
+  })
 }
