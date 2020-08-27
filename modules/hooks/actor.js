9ddbf85922155071047b103746a983bd98079967
Russell <russellthurman94@gmail.com>
2020-08-27 18:10:59 -0500

    new DSN dice images

--- modules/hooks/actor.js
+++ modules/hooks/actor.js
@@ -5,15 +5,16 @@ export default function() {
   Hooks.on("preCreateActor", (createData) => {
 
     // Set wounds, advantage, and display name visibility
-    mergeObject(createData,
-      {
-        "token.bar1": { "attribute": "status.wounds" },                 // Default Bar 1 to Wounds
-        "token.bar2": { "attribute": "status.advantage" },               // Default Bar 2 to Advantage
-        "token.displayName": CONST.TOKEN_DISPLAY_MODES.OWNER_HOVER,    // Default display name to be on owner hover
-        "token.displayBars": CONST.TOKEN_DISPLAY_MODES.OWNER_HOVER,    // Default display bars to be on owner hover
-        "token.disposition": CONST.TOKEN_DISPOSITIONS.NEUTRAL,         // Default disposition to neutral
-        "token.name": createData.name                                       // Set token name to actor name
-      })
+    if (!createData.token)
+      mergeObject(createData,
+        {
+          "token.bar1": { "attribute": "status.wounds" },                 // Default Bar 1 to Wounds
+          "token.bar2": { "attribute": "status.advantage" },               // Default Bar 2 to Advantage
+          "token.displayName": CONST.TOKEN_DISPLAY_MODES.OWNER_HOVER,    // Default display name to be on owner hover
+          "token.displayBars": CONST.TOKEN_DISPLAY_MODES.OWNER_HOVER,    // Default display bars to be on owner hover
+          "token.disposition": CONST.TOKEN_DISPOSITIONS.NEUTRAL,         // Default disposition to neutral
+          "token.name": createData.name                                       // Set token name to actor name
+        })
 
     // Set custom default token
     if (!createData.img)
