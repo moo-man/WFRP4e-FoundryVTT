b4d3b00d1aa9077a75280844d18c606d6fb1c606
Russell <russellthurman94@gmail.com>
2020-11-30 09:09:51 -0600

    Added sidebar buttons for wfrp4e actions

--- modules/hooks/sidebar.js
+++ modules/hooks/sidebar.js
@@ -1,4 +1,5 @@
 import BugReportFormWfrp4e from "../apps/bug-report.js";
+import GeneratorWfrp4e from "../apps/char-gen.js";
 
 export default function() {
   Hooks.on("renderSidebarTab", async (app, html) => {
@@ -11,6 +12,33 @@ export default function() {
       })
   
       button.insertAfter(html.find("#game-details"))
+      
+    }
+
+    if (app.options.id == "tables")
+    {
+      let button = $(`<button class='wfrp4e-tables'>${game.i18n.localize("WFRP4e Tables")}</button>`)
+  
+      button.click(ev => {
+        ui.sidebar.activateTab("chat")
+        ChatMessage.create({content : game.wfrp4e.tables.tableMenu()})
+      })
+  
+      button.insertAfter(html.find(".header-actions"))
+      
+    }
+
+    if (app.options.id == "actors")
+    {
+      let button = $(`<button class='generate-character'>${game.i18n.localize("Generate Character")}</button>`)
+  
+      button.click(ev => {
+        ui.sidebar.activateTab("chat")
+        Actor.create({type : "character", name : "New Character"}, {renderSheet: true} )
+        GeneratorWfrp4e.speciesStage();
+      })
+  
+      button.insertAfter(html.find(".header-actions"))
       
     }
   })
