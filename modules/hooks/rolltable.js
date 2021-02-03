06a366b02badc40ea78ad3f9c4c623542048d7e1
Russell <russellthurman94@gmail.com>
2021-02-02 21:58:59 -0600

    Fixed table styling

--- modules/hooks/rolltable.js
+++ modules/hooks/rolltable.js
@@ -1,6 +1,5 @@
 export default function() {
-  // Fixes width for roll tables with our custom UI
-  Hooks.on("renderRollTableConfig", async (obj, html, data) => {
-    html[0].style.width = "730px"
-  });
+    Hooks.on("preCreateTableResult", (table, result) => {
+        result.img = "icons/svg/d10-grey.svg"
+    })
 }
