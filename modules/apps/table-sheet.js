95fdd88d8c34ce79ed9d45dad6fce090deba2799
Russell <russellthurman94@gmail.com>
2021-03-18 20:44:43 -0500

    finished table sheet

--- modules/apps/table-sheet.js
+++ modules/apps/table-sheet.js
@@ -14,7 +14,8 @@ export default class WFRP4eTableSheet extends Application {
     options.height = 900;
     options.width = 600;
     options.minimizable = true;
-    options.title = "WFRP4e Table"
+    options.title = "WFRP4e Table";
+    options.tabs = [{ navSelector: ".tabs", contentSelector: ".content" }]
     return options;
   }
 
@@ -29,8 +30,32 @@ export default class WFRP4eTableSheet extends Application {
     let data = super.getData();
     console.log(this.table)
     data.table = this.table;
+    data.columns = this.table.columns || this.table.multi
+
+    if (data.columns) {
+      data.table.columnRows = {}
+      data.columns.forEach(c => {
+        data.table.columnRows[c] = duplicate(data.table.rows).map(r => {
+          r.range = r.range[c]
+          r.rangeLabel = r.range[0]
+          if (r.range.length > 1)
+            r.rangeLabel += ` - ${r.range[1]}`
+
+          if (this.table.columns)
+            r.label = r.name || r.description
+          else if (this.table.multi)
+            r.label = r[c].name || r[c].description
+
+          r.label = TextEditor.enrichHTML(r.label)
+          return r
+        })
+        data.table.columnRows[c] = data.table.columnRows[c].filter(r => r.range.length)
+      })
+    }
     data.table.rows.forEach(r => {
-      r.rangeLabel = `${r.range[0]} - ${r.range[1]}`
+      r.rangeLabel = r.range[0]
+      if (r.range.length > 1)
+        r.rangeLabel += ` - ${r.range[1]}`
       r.label = r.name || r.description
       r.label = TextEditor.enrichHTML(r.label)
     })
@@ -48,8 +73,12 @@ export default class WFRP4eTableSheet extends Application {
     
     html.find(".row-label").click(ev => {
       let index = Number(ev.target.dataset.index);
+      let column = ev.target.dataset.column
       let lookup = this.table.rows[index].range[0]
-      game.wfrp4e.tables.rollToChat(this.table.key, {lookup})
+      if (column)
+        lookup = this.table.columnRows[column][index].range[0]
+        
+      game.wfrp4e.tables.rollToChat(this.table.key, {lookup}, column)
     })
   }
 }
