ff0c0099b3b27df02fe9a818c8cab0d419ff1899
Russell <russellthurman94@gmail.com>
2021-05-20 21:04:21 -0500

    Custom table icons

--- modules/hooks/rolltable.js
+++ modules/hooks/rolltable.js
@@ -2,4 +2,8 @@ export default function() {
     Hooks.on("preCreateTableResult", (table, result) => {
         result.img = "icons/svg/d10-grey.svg"
     })
+
+    Hooks.on("preCreateRollTable", (table) => {
+        table.img = "systems/wfrp4e/ui/buttons/d10.png"
+    })
 }
