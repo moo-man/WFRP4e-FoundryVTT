9e248385cf7c80cee4da4d652ff5a776d1a029d9
Russell <russellthurman94@gmail.com>
2021-08-04 23:48:42 -0500

    Fix Ranged Trait damage, fix create basic skill button
    fix #759

--- modules/item/item-wfrp4e.js
+++ modules/item/item-wfrp4e.js
@@ -1682,7 +1682,7 @@ export default class ItemWfrp4e extends Item {
     else if (this.type == "trait" && this.rollable.damage)
       damage = this.Specification
 
-    return damage
+    return parseInt(damage)
   }
 
   get DamageString() {
