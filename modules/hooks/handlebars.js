eccd9c5d4954502d004997bd2068e8d4d6f2b320
Silent_Mark <marek.kembrowski@gmail.com>
2022-05-18 21:04:30 +0200

    white spaces

--- modules/hooks/handlebars.js
+++ modules/hooks/handlebars.js
@@ -16,7 +16,7 @@ export default function () {
         Handlebars.registerHelper("configLookup", function (obj, key) {
             return game.wfrp4e.config[obj][key]
         })
-        
+
         Handlebars.registerHelper("array", function (array, cls) {
             if (typeof cls == "string")
                 return array.map(i => `<a class="${cls}">${i}</a>`).join(`<h1 class="${cls} comma">, </h1>`)
