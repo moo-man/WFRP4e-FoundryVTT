453f74fdb02bc018e767bac27d8bdb3ac9e388af
Russell <russellthurman94@gmail.com>
2022-07-31 14:04:35 -0500

    Fix ammo error using Ranged Trait Fixes #1193

--- modules/item/item-wfrp4e.js
+++ modules/item/item-wfrp4e.js
@@ -1606,7 +1606,7 @@ export default class ItemWfrp4e extends Item {
   }
 
   get ammo() {
-    if (this.attackType == "ranged" && this.currentAmmo.value && this.isOwned)
+    if (this.attackType == "ranged" && this.currentAmmo?.value && this.isOwned)
       return this.actor.items.get(this.currentAmmo.value)
   }
 
