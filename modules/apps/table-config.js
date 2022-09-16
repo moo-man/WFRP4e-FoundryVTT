78d2fa72510f3620edc08ac2156aede43ca9fd5d
Russell <russellthurman94@gmail.com>
2022-09-16 15:15:58 -0500

    Can't edit Table Key and Table Column Add key/column fields instead of overwriting entire template Fixes #1236

--- modules/apps/table-config.js
+++ modules/apps/table-config.js
@@ -6,7 +6,15 @@ export default class WFRPTableConfig extends RollTableConfig {
         return mergeObject(super.defaultOptions, {width: 725})
     }
 
-    get template() {
-        return "systems/wfrp4e/templates/apps/table-config.html"
+    activateListeners(html) 
+    {
+        super.activateListeners(html);
+
+        html.prepend($(`<div class="form-group">
+            <label>${game.i18n.localize("TABLE.Key")}</label>
+            <input type="text" name="flags.wfrp4e.key" value="${this.object.flags.wfrp4e?.key || ""}"/>
+            <label>${game.i18n.localize("TABLE.Column")}</label>
+            <input type="text" name="flags.wfrp4e.column" value="${this.object.flags.wfrp4e?.column || ""}"/>
+        </div>`))
     }
 }
