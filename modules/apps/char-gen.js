f1b98eee3e168645190bde6af96ff2d81fecd0cd
Russell <russellthurman94@gmail.com>
2022-10-13 22:16:22 -0500

    trapping stage

--- modules/apps/char-gen.js
+++ modules/apps/char-gen.js
@@ -1,5 +1,6 @@
 import NameGenWfrp from "./name-gen.js";
 import WFRP_Utility from "../system/utility-wfrp4e.js";
+import ItemWfrp4e from "../item/item-wfrp4e.js";
 
 
 /**
@@ -72,7 +73,8 @@ export default class CharGenWfrp4e extends Application{
         completed : false
       },
       {
-        app : { options: {title : game.i18n.localize("CHARGEN.StageTrappings")}},//new AttributesStage(this.data, this.next.bind(this), this.update.bind(this)),
+        class : TrappingStage,
+        app : null,
         key: "trappings",
         dependantOn : ["career"],
         completed : false
@@ -958,4 +960,122 @@ class SkillsTalentsStage extends ChargenStage
     this.render(true)
   }
 
+}
+
+class TrappingStage extends ChargenStage
+{
+  static get defaultOptions() {
+    const options = super.defaultOptions;
+    options.resizable = true;
+    options.width = 450;
+    options.height = 600;
+    options.classes.push("skills-talents")
+    options.minimizable = true;
+    options.title = game.i18n.localize("CHARGEN.StageTrappings")
+    return options;
+  }
+
+  static get title() { return game.i18n.localize("CHARGEN.StageTrappings")}
+
+  get template() {
+    return "systems/wfrp4e/templates/apps/chargen/trappings.html";
+  } 
+
+
+
+  constructor(...args)
+  {
+    super(...args)
+
+    this.context.classStrings = game.wfrp4e.config.classTrappings[this.data.items.career.class.value + "s"].split(",")
+    this.context.careerStrings = this.data.items.career.trappings
+
+    this.context.class = Promise.all(game.wfrp4e.config.classTrappings[this.data.items.career.class.value + "s"].split(",").map(i => WFRP_Utility.find(i.trim(), "trapping")))
+    this.context.career = Promise.all(this.data.items.career.trappings.map(i => WFRP_Utility.find(i, "trapping")))
+  }
+
+
+  context = {
+    items : [],
+    class : [],
+    career : [],
+    missing : null,
+    added : []
+  }
+
+
+  async getData() {
+    let data = await super.getData();
+    this.context.class = await this.context.class
+    this.context.career = await this.context.career
+
+
+    if (!this.context.missing)
+    {
+      this.context.missing = [];
+
+      this.context.class.forEach((trapping, i) => {
+        if (!trapping)
+        {
+          this.context.missing.push({
+            string : this.context.classStrings[i],
+            choice : "keep"
+          })
+        }
+      })
+
+      this.context.career.forEach((trapping, i) => {
+        if (!trapping)
+        {
+          this.context.missing.push({
+            string : this.context.careerStrings[i],
+            choice : "keep"
+          })
+        }
+      })
+    }
+
+
+
+    this.context.class = this.context.class.filter(i => i)
+    this.context.career = this.context.career.filter(i => i)
+    return data
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html)
+    const dragDrop = new DragDrop({
+      dropSelector: '.chargen-content',
+      permissions: { drop: () => true },
+      callbacks: { drop: this._onDrop.bind(this) },
+    });
+
+    dragDrop.bind(html[0]);
+
+    html.find(".missing-trapping-choice").click(ev => {
+      let name =  ev.currentTarget.name
+      let index = Number(name.split("-")[1])
+      this.context.missing[index].choice = ev.currentTarget.value
+      this.render(true)
+    })
+  }
+
+  async _onDrop(ev)
+  {
+    let dragData = JSON.parse(ev.dataTransfer.getData("text/plain"));
+
+    if (dragData.type == "Item")
+    {
+      this.context.added.push(await Item.implementation.fromDropData(dragData))
+    }
+    this.render(true)
+  }
+
+
+  _updateObject(ev, formData)
+  {
+    let missing = this.context.missing.filter(i => i.choice == "keep").map(i => new ItemWfrp4e({name : i.string, type : "trapping", system : {"trappingType.value" : "misc"}}))
+
+    this.data.items.trappings = missing.concat(this.context.class, this.context.career, missing, this.context.added)
+  }
 }
