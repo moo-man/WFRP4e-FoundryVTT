d7d5ae3537564e960e9dae1d4c81ccbe9a83127e
Russell <russellthurman94@gmail.com>
2023-03-17 16:02:07 -0500

    account for flag damage increase in traits

--- modules/item/item-wfrp4e.js
+++ modules/item/item-wfrp4e.js
@@ -1933,7 +1933,7 @@ export default class ItemWfrp4e extends Item {
     else if (this.type == "weapon")                                                                                                                      // Account for Durable, Math.max so durable doesn't go past damageToItem
       damage = this.applyAmmoMods(this.computeWeaponFormula("damage"), "damage") + (this.actor.flags[`${this.attackType}DamageIncrease`] || 0) - Math.max((this.damageToItem.value - (this.properties.qualities.durable?.value || 0)), 0)
     else if (this.type == "trait" && this.rollable.damage)
-      damage = this.Specification
+      damage = this.Specification + (this.actor.flags[`${this.attackType}DamageIncrease`] || 0)
 
 
     //@HOUSE
