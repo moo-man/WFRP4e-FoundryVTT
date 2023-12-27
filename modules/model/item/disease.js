a3b72a6c2f261f37eb87d328d3513bd10f1747d0
Forien <mr.forien@gmail.com>
2023-12-27 20:40:21 +0100

    Disease code refactor (Part 1) (#1851)
    * part 1 of disease refactor
    
    * awaiting async test.roll()
    
    * updated UUIDs to v7+ items compendium
    
    * changed duplicate to deepClone
    
    * fixing DiseaseModel not having Base Schema
    
    * fixed my own oopsie
    
    * auto formatting
    
    * fixed diseases applying symptom effects regardless of disease state
    
    * simplified disease updates by moving them upstream and fixed minor bugs

--- modules/model/item/disease.js
+++ modules/model/item/disease.js
@@ -1,59 +1,67 @@
-import { BaseItemModel } from "./components/base";
+import {BaseItemModel} from "./components/base";
+
 let fields = foundry.data.fields;
 
 /**
  * Represents an Item used by both Patrons and Characters/NPCs
  */
-export class DiseaseModel extends BaseItemModel
-{
+export class DiseaseModel extends BaseItemModel {
 
-    static defineSchema() 
-    {
-        let schema = {};
-        schema.contraction = new fields.SchemaField({
-            value : new fields.StringField(),
-        });
-        
-        schema.incubation = new fields.SchemaField({
-            value : new fields.StringField(),
-        })
+  static defineSchema() {
+    let schema = super.defineSchema();
+    schema.contraction = new fields.SchemaField({
+      value: new fields.StringField(),
+    });
 
-        schema.duration = new fields.SchemaField({
-            value : new fields.StringField(),
-            unit : new fields.StringField(),
-            active : new fields.BooleanField(),
-        })
+    schema.incubation = new fields.SchemaField({
+      value: new fields.StringField(),
+    });
 
-        schema.symptoms = new fields.SchemaField({
-            value : new fields.StringField(),
-        })
+    schema.duration = new fields.SchemaField({
+      value: new fields.StringField(),
+      unit: new fields.StringField(),
+      active: new fields.BooleanField(),
+    });
 
-        schema.permanent = new fields.SchemaField({
-            value : new fields.StringField(),
-        })
-        return schema;
-    }
+    schema.symptoms = new fields.SchemaField({
+      value: new fields.StringField(),
+    });
 
-    async expandData(htmlOptions) {
-        let data = await super.expandData(htmlOptions);
-        data.properties.push(`<b>${game.i18n.localize("Contraction")}:</b> ${this.contraction.value}`);
-        data.properties.push(`<b>${game.i18n.localize("Incubation")}:</b> ${this.incubation.value} ${this.incubation.unit}`);
-        data.properties.push(`<b>${game.i18n.localize("Duration")}:</b> ${this.duration.value} ${this.duration.unit}`);
-        data.properties = data.properties.concat(this.parent.effects.map(i => i = "<a class ='symptom-tag'><i class='fas fa-user-injured'></i> " + i.name.trim() + "</a>").join(", "));
-        if (this.permanent.value)
-          data.properties.push(`<b>${game.i18n.localize("Permanent")}:</b> ${this.permanent.value}`);
-        return data;
-      }
+    schema.permanent = new fields.SchemaField({
+      value: new fields.StringField(),
+    });
 
-      chatData()
-      {
-        let properties = [];
-        properties.push(`<b>${game.i18n.localize("Contraction")}:</b> ${this.contraction.value}`);
-        properties.push(`<b>${game.i18n.localize("Incubation")}:</b> <a class = 'chat-roll'><i class='fas fa-dice'></i> ${this.incubation.value}</a>`);
-        properties.push(`<b>${game.i18n.localize("Duration")}:</b> <a class = 'chat-roll'><i class='fas fa-dice'></i> ${this.duration.value}</a>`);
-        properties.push(`<b>${game.i18n.localize("Symptoms")}:</b> ${(this.symptoms.value.split(",").map(i => i = "<a class ='symptom-tag'><i class='fas fa-user-injured'></i> " + i.trim() + "</a>")).join(", ")}`);
-        if (this.permanent.value)
-          properties.push(`<b>${game.i18n.localize("Permanent")}:</b> ${this.permanent.value}`);
-        return properties;
-      }
+    return schema;
+  }
+
+  async expandData(htmlOptions) {
+    let data = await super.expandData(htmlOptions);
+    data.properties.push(`<b>${game.i18n.localize("Contraction")}:</b> ${this.contraction.value}`);
+    data.properties.push(`<b>${game.i18n.localize("Incubation")}:</b> ${this.incubation.value} ${this.incubation.unit}`);
+    data.properties.push(`<b>${game.i18n.localize("Duration")}:</b> ${this.duration.value} ${this.duration.unit}`);
+    data.properties = data.properties.concat(this.parent.effects.map(i => i = "<a class ='symptom-tag'><i class='fas fa-user-injured'></i> " + i.name.trim() + "</a>").join(", "));
+
+    if (this.permanent.value)
+      data.properties.push(`<b>${game.i18n.localize("Permanent")}:</b> ${this.permanent.value}`);
+
+    return data;
+  }
+
+  chatData() {
+    let properties = [];
+    properties.push(`<b>${game.i18n.localize("Contraction")}:</b> ${this.contraction.value}`);
+    properties.push(`<b>${game.i18n.localize("Incubation")}:</b> <a class = 'chat-roll'><i class='fas fa-dice'></i> ${this.incubation.value}</a>`);
+    properties.push(`<b>${game.i18n.localize("Duration")}:</b> <a class = 'chat-roll'><i class='fas fa-dice'></i> ${this.duration.value}</a>`);
+    properties.push(`<b>${game.i18n.localize("Symptoms")}:</b> ${(this.symptoms.value.split(",").map(i => i = "<a class ='symptom-tag'><i class='fas fa-user-injured'></i> " + i.trim() + "</a>")).join(", ")}`);
+
+    if (this.permanent.value)
+      properties.push(`<b>${game.i18n.localize("Permanent")}:</b> ${this.permanent.value}`);
+
+    return properties;
+  }
+
+  shouldTransferEffect(effect)
+  {
+    return this.duration.active === true;
+  }
 }
