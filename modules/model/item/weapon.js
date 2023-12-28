99c92d2e73259794b2b5d2cf7f56604e9c581c01
Forien <mr.forien@gmail.com>
2023-12-28 08:48:39 +0100

    fixed setProperty arguments (takes 3 args, not an object of 3)

--- modules/model/item/weapon.js
+++ modules/model/item/weapon.js
@@ -158,7 +158,7 @@ export class WeaponModel extends PropertiesMixin(PhysicalItemModel) {
         let preCreateData = await super.preCreateData(data, options, user);
 
         if (this.parent.isOwned && this.parent.actor.type != "character" && this.parent.actor.type != "vehicle") {
-            setProperty({ preCreateData, "system.equipped": true }); // TODO: migrate this into a unified equipped property 
+            foundry.utils.setProperty(preCreateData, "system.equipped", true); // TODO: migrate this into a unified equipped property
         }
 
         return preCreateData;
