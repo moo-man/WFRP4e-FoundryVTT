aca959ee50b0b0f05747d6776c033ff14ed047d5
Russell <russellthurman94@gmail.com>
2024-04-25 21:04:26 -0500

    edit robe/staff scripts

--- scripts/PeZYj8b0vedyJj00.js
+++ scripts/PeZYj8b0vedyJj00.js
@@ -3,20 +3,11 @@
 let mainEffect = this.item.effects.contents[0];
 if (mainEffect.name.includes("(Lore)"))
 {
-    let choice = await ItemDialog.create(ItemDialog.objectToArray({
-        beasts : game.wfrp4e.config.magicLores["beasts"],
-        death : game.wfrp4e.config.magicLores["death"],
-        fire : game.wfrp4e.config.magicLores["fire"],
-        heavens : game.wfrp4e.config.magicLores["heavens"],
-        metal : game.wfrp4e.config.magicLores["metal"],
-        life : game.wfrp4e.config.magicLores["life"],
-        light : game.wfrp4e.config.magicLores["light"],
-        shadow : game.wfrp4e.config.magicLores["shadow"]
-        }, this.item.img), 1, "Choose Lore");
+    let choice = await ItemDialog.create(ItemDialog.objectToArray(game.wfrp4e.config.magicLores, this.item.img), 1, "Choose Lore");
     if (choice.length)
     {
-        mainEffect.updateSource({name : mainEffect.name.replace("Lore", choice[0].name)})
-        this.item.updateSource({name : this.item.name += ` (${choice[0].name})`})
+        mainEffect.update({name : mainEffect.name.replace("Lore", choice[0].name)})
+        this.item.update({name : this.item.name += ` (${choice[0].name})`})
     }
 }
 
--- scripts/UsuwsmU1TUQLQVM2.js
+++ scripts/UsuwsmU1TUQLQVM2.js
@@ -1,1 +1,2 @@
-return !args.spell
+let lore = this.effect.name.split("(")[1].split(")")[0].toLowerCase();
+return !args.spell || (args.type == "cast" && ["petty", lore].includes(args.spell.system.lore.value));
