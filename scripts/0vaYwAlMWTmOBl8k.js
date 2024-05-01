dde4fa2c38b18990bc322ff4217b468c87f87c19
Russell <russellthurman94@gmail.com>
2024-04-30 22:15:23 -0500

    close #1971

--- scripts/0vaYwAlMWTmOBl8k.js
+++ scripts/0vaYwAlMWTmOBl8k.js
@@ -1,3 +1,7 @@
-args.fields.modifier += 20;
-args.fields.successBonus++;
-args.fields.hitLocation = "head";
+if (!args.flags.strikeToStun)
+{
+    args.flags.strikeToStun = true
+    args.fields.modifier += 20;
+    args.fields.successBonus++;
+    args.fields.hitLocation = "head";
+}
