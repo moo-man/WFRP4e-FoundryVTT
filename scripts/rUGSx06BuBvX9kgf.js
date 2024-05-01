27cd2462f7b045ea7b90db5c180ca372e0fed8eb
Txus <87753744+Txus5012@users.noreply.github.com>
2024-05-01 04:53:10 +0200

    Resolute breakdown message (#1975)

--- scripts/rUGSx06BuBvX9kgf.js
+++ scripts/rUGSx06BuBvX9kgf.js
@@ -2,4 +2,8 @@ if (args.test.result.charging)
 {
    args.test.result.damage += 1
    args.test.result.additionalDamage += 1
+   if (!args.test.result.resolute) {
+      args.test.result.breakdown.damage.other.push({label : this.effect.name, value : this.item.Advances});
+      args.test.result.resolute = true // Prevent duplicate messages
+     }
 }
