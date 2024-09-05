6708eaa2be1ce335656314a5f2415db35208d81f
Russell <russellthurman94@gmail.com>
2024-09-05 09:25:46 -0500

    fix migrations

--- modules/system/migrations.js
+++ modules/system/migrations.js
@@ -306,7 +306,7 @@ export default class Migration {
     }
     effectModels = effectModels.filter(e => !foundry.utils.isEmpty(e));
 
-    await this.actor.updateEmbeddedDocuments("ActiveEffect", effectModels);
+    await actor.updateEmbeddedDocuments("ActiveEffect", effectModels);
   }
 
   static migrateJournalData(journal)
