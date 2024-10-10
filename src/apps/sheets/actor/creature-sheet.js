5201d775596045c798e168d1c008843259404496
Russell <russellthurman94@gmail.com>
2024-10-09 22:47:36 -0500

    merge light mode

--- src/apps/sheets/actor/creature-sheet.js
+++ src/apps/sheets/actor/creature-sheet.js
@@ -16,6 +16,7 @@ export default class ActorSheetWFRP4eCreatureV2 extends StandardWFRP4eActorSheet
         header : {scrollable: [""], template : 'systems/wfrp4e/templates/sheets/actor/characteristic-header.hbs', classes: ["sheet-header"] },
         tabs: { scrollable: [""], template: 'systems/wfrp4e/templates/sheets/actor/actor-tabs.hbs' },
         main: { scrollable: [""], template: 'systems/wfrp4e/templates/sheets/actor/creature/creature-main.hbs'},
+        skills: { scrollable: [""], template: 'systems/wfrp4e/templates/sheets/actor/tabs/actor-skills.hbs' },
         talents: { scrollable: [""], template: 'systems/wfrp4e/templates/sheets/actor/tabs/actor-talents.hbs' },
         combat: { scrollable: [""], template: 'systems/wfrp4e/templates/sheets/actor/tabs/actor-combat.hbs' },
         effects: { scrollable: [""], template: 'systems/wfrp4e/templates/sheets/actor/tabs/actor-effects.hbs' },
@@ -23,49 +24,6 @@ export default class ActorSheetWFRP4eCreatureV2 extends StandardWFRP4eActorSheet
         religion: { scrollable: [""], template: 'systems/wfrp4e/templates/sheets/actor/tabs/actor-religion.hbs' },
         trappings: { scrollable: [""], template: 'systems/wfrp4e/templates/sheets/actor/tabs/actor-inventory.hbs' },
         notes: { scrollable: [""], template: 'systems/wfrp4e/templates/sheets/actor/npc/npc-notes.hbs' },
-      }
-
-      static TABS = {
-        main: {
-          id: "main",
-          group: "primary",
-          label: "Main",
-        },
-        talents: {
-          id: "talents",
-          group: "primary",
-          label: "Talents",
-        },
-        combat: {
-          id: "combat",
-          group: "primary",
-          label: "Combat",
-        },
-        effects: {
-          id: "effects",
-          group: "primary",
-          label: "Effects",
-        },
-        religion: {
-          id: "religion",
-          group: "primary",
-          label: "Religion",
-        },
-        magic: {
-          id: "magic",
-          group: "primary",
-          label: "Magic",
-        },
-        trappings: {
-          id: "trappings",
-          group: "primary",
-          label: "Trappings",
-        },
-        notes: {
-          id: "notes",
-          group: "primary",
-          label: "Notes",
-        }
       }
       
       async _prepareContext(options)
