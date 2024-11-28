6bf66f47f74ca39e520cc92516c547a87283107e
Marek Kembrowski <marek.kembrowski@se.gt.com>
2024-11-28 23:56:38 +0100

    revert change

--- modules/hooks/settings.js
+++ modules/hooks/settings.js
@@ -7,8 +7,7 @@ export default function() {
         // Then, make sure that change is reflected in the counter on the combat tracker (if the update was made by a different user)
         if (setting.key == "wfrp4e.groupAdvantageValues")
         {
-            let message = game.i18n.format("GroupAdvantageUpdated", {players : setting.value.players, enemies : setting.value.enemies});
-            ui.notifications.notify(message)
+            ui.notifications.notify(game.i18n.format("GroupAdvantageUpdated", {players : setting.value.players, enemies : setting.value.enemies}))
             if (game.user.isGM && game.combat)
             {
                 // This sorta sucks because there isn't a way to update both actors and synthetic actors in one call
@@ -18,13 +17,12 @@ export default function() {
                 })
             }
             // Update counter values, can't just use ui.combat because there might be popped out combat trackers
-            let trackers = [ui.combat].concat(Object.values(ui.windows).filter(w => w instanceof CombatTracker));
-            for (let tracker of trackers) {
+            [ui.combat].concat(Object.values(ui.windows).filter(w => w instanceof CombatTracker)).forEach(tracker => {
                 tracker.element.find(".advantage-group input").each((index, input) => {
                     let group = input.dataset.group
                     input.value = setting.value[group]
                 })
-            }
+            })
         }
     })
 
