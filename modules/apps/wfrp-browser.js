2161bc9d6d0aea8c317a75cf79b2f67393200dc5
Forien <mr.forien@gmail.com>
2025-03-24 22:46:51 +0100

    Added missing localization entries for Compendium Browser (#2419)
    * initial pass of new compendium browser
    
    * finished filters for Item models
    
    * actor filters
    
    * fix skill type filter
    
    * Remove WFRP4e browser, its template and associated styles
    
    * add missing localization entries
    
    * adjust the deprecation warning

--- modules/apps/wfrp-browser.js
+++ modules/apps/wfrp-browser.js
@@ -3,7 +3,7 @@ export default class BrowserWfrp4e extends warhammer.apps.CompendiumBrowser {
   constructor(...args) {
     super(...args)
 
-    const msg = "BrowserWfrp4e has been removed in favor of the Compendium Browser. Please use `new warhammer.apps.CompendiumBrowser().render(true)` instead.";
+    const msg = "BrowserWfrp4e has been removed in favor of the Compendium Browser. Please use `warhammer.apps.CompendiumBrowser.render(true);` instead.";
     ui.notifications.warn(msg, {permanent: true});
   }
 
