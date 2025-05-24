da95fd540a5ea84ac36d94765e37fafef0f7a83d
Russell <russellthurman94@gmail.com>
2025-05-24 18:29:35 -0500

    add table key config to header close #2493

--- src/hooks/rolltable.js
+++ src/hooks/rolltable.js
@@ -8,4 +8,47 @@ export default function() {
         if (!data.img)
             table.updateSource({"img" : "systems/wfrp4e/ui/buttons/d10.webp"})
     })
+
+    Hooks.on("getHeaderControlsRollTableSheet", (sheet, controls) => {
+        controls.push({
+            label : "Configure Table Key",
+            icon : "fa-solid fa-key",
+            action : "configureKey" 
+        });
+        sheet.options.actions.configureKey = function (ev, target){
+            let key = this.document.getFlag("wfrp4e", "key");
+            let column = this.document.getFlag("wfrp4e", "column");
+
+            new foundry.applications.api.DialogV2({
+              window: { title: "Table Key & Column" },
+              content: `
+              <div class="form-group">
+                <label>Key</label>
+              <div class="form-fields">
+              <input type="text" value="${key}" name="key"/>
+              </div>
+              </div>
+
+              <div class="form-group">
+                <label>Column</label>
+              <div class="form-fields">
+              <input type="text" value="${column}" name="column"/>
+              </div>
+              </div>
+              `,
+              buttons: [{
+                action: "submit",
+                label: "Submit",
+                default: true,
+                callback: (event, button, dialog) => {
+                    let key = button.form.elements.key.value;
+                    let column = button.form.elements.column.value;
+
+                    this.document.update({"flags.wfrp4e" : {key, column}})
+                }
+              }],
+            }).render({ force: true });
+        }
+        sheet.options.actions.configureKey = sheet.options.actions.configureKey.bind(sheet);
+    })
 }
