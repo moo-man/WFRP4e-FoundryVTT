427f10401e82005e21b4e91f04bd1f4ccc2d174b
Michał Szura <michal.szura@gmail.com>
2025-08-14 00:26:16 +0200

    refactor: Breakout sort into series of filters

--- src/apps/career-selector.js
+++ src/apps/career-selector.js
@@ -63,7 +63,10 @@ export default class CareerSelector extends  HandlebarsApplicationMixin(Applicat
         const currentCareerGroup = this.currentCareer.system.careergroup.value;
         this.careers = await warhammer.utility.findAllItems("career", game.i18n.localize("CAREER.Loading"), true, ["system.careergroup.value", "system.level.value", "system.class.value"])
         this.careers = this.careers.sort((a, b) => a.system.careergroup.value > b.system.careergroup.value ? 1 : -1)
-            .sort((a, b) => a.system.careergroup.value === currentCareerGroup ? -1 : b.system.careergroup.value === currentCareerGroup ? 1 : 0)
+        const currentCareers = this.careers.filter((a) => a.system.careergroup.value === currentCareerGroup)
+        this.careers = this.careers.filter((a) => a.system.careergroup.value !== currentCareerGroup)
+        this.careers.unshift(...currentCareers)
+
         this.careers = this.careers.filter(i =>
         {
             if (game.user.isGM)
