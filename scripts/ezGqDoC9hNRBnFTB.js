d6a19ce57631664bdf2db515d643539b19e92e4e
Russell <russellthurman94@gmail.com>
2026-02-14 20:50:42 -0600

    fix various scripts close #2542

--- scripts/ezGqDoC9hNRBnFTB.js
+++ scripts/ezGqDoC9hNRBnFTB.js
@@ -1,5 +1,5 @@
 let choices = await Promise.all([warhammer.utility.findItemId("1zaqojk0Oq1m8vYv"), warhammer.utility.findItemId("zIuarD5mB0EF0ji0")])
-let items = await game.wfrp4e.apps.ItemDialog.create(choices, 1, "Choose a Weapon")
+let items = await ItemDialog.create(choices, 1, {title: this.effect.name, text: "Choose a Weapon"})
 items = items.map(i => i.toObject())
 
 items.forEach(i => i.system.equipped.value = true);
