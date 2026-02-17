2e39bae6b347b3aafca622f8428da2dcc257a6db
Russell <russellthurman94@gmail.com>
2026-02-16 21:36:00 -0600

    close #2715

--- scripts/fSDFCHAo97RLpwGU.js
+++ scripts/fSDFCHAo97RLpwGU.js
@@ -1,9 +1,15 @@
 const hitLocation = args.test.hitloc.result
-const hitLocationArmour = args.test.targets[0].armour[hitLocation]
+const hitLocationArmour = args.test.targets[0]?.armour[hitLocation]
 
-if (hitLocationArmour.layers.length > 0) {
-  hitLocationArmour.layers.forEach(layer => {
-    layer.source.system.qualities.value = [];
-    layer.impenetrable = false;
-  });
+if (hitLocationArmour)
+{
+  let qualities = [];
+  for (let layer of hitLocationArmour.layers) 
+  {
+    qualities = qualities.concat(layer.source.system.qualities.value);
+  }
+
+  qualities = new Set(qualities); // remove duplicates;
+
+  this.script.message(`Ignores ${Array.from(qualities).map(i => game.wfrp4e.config.armorQualities[i.name]).join(", ")}`);
 }
