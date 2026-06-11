bf33d55cd6b5c02053b7c4879b582170256bddec
Michał Szura <michal.szura@gmail.com>
2026-06-11 23:06:29 +0200

    refactor: extract spellcaster armour penalty into shared helper, add homebrew toggle
    - Extract duplicate computeFields() logic from CastDialog and ChannellingDialog
      into applySpellcasterArmourPenalty() in spellcaster-armour-penalty.js
    - Remove accidental CastTest import from cast-dialog.js
    - Add spellcasterArmourPenalties homebrew setting (default: true / enabled)
    - Add en.json keys for the new setting with rulebook reference (CB p.237)

--- src/apps/roll-dialog/channelling-dialog.js
+++ src/apps/roll-dialog/channelling-dialog.js
@@ -1,5 +1,6 @@
 import ChannelTest from "../../system/rolls/channel-test";
 import SkillDialog from "./skill-dialog";
+import { applySpellcasterArmourPenalty } from "./spellcaster-armour-penalty.js";
 
 export default class ChannellingDialog extends SkillDialog {
 
@@ -107,33 +108,7 @@ export default class ChannellingDialog extends SkillDialog {
 
     computeFields() {
         super.computeFields();
-        let talentMetal = this.actor.has("Arcane Magic (Metal)", "talent");
-        let talentBeasts = this.actor.has("Arcane Magic (Beasts)", "talent");
-        let wearingArmour = {
-            head: 0,
-            lArm: 0,
-            rArm: 0,
-            lLeg: 0,
-            rLeg: 0,
-            body: 0,
-        };
-        let debuff = 0;
-        for (let a of this.actor.itemTags["armour"].filter(i => i.isEquipped)) {
-            if ((a.system.isMetal && !talentMetal) || (a.system.isLeather && !talentBeasts)) {
-                const currentAP = a.system.currentAP;
-                for (let loc in currentAP) {
-                    wearingArmour[loc] = currentAP[loc] + wearingArmour[loc];
-                }
-            }
-        }
-        for (let loc in wearingArmour) {
-            debuff = Math.max(debuff, wearingArmour[loc]);
-        }
-
-        if (debuff > 0) {
-            this.fields.slBonus += -debuff;
-            this.tooltips.add("slBonus", -debuff, game.i18n.localize("SHEET.SpellcasterArmourPenalties"));
-        }
+        applySpellcasterArmourPenalty(this.actor, this.fields, this.tooltips);
     }
 
     _getSubmissionData()
